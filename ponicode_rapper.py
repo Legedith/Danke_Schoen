d1c2edf1deb0b4086d079394779f5f4e4f1eb9cf
Christopher Nguyen <61757292+cnguyen-uk@users.noreply.github.com>
2020-10-05 21:47:24 +0100

    General code clean up and readability improvements
    Remove unused ast package; reformat code to stay within 79 character-per-line standard; improve docstrings to follow PEP 8 standards

--- ponicode_rapper.py
+++ ponicode_rapper.py
@@ -1,7 +1,6 @@
 import random
-import markovify #uses markov models to generate new sentences
-import ast  
-from rhyme import rhyme_finder #uses cmudict to find rhyming words based on phonetics
+import markovify  # Use Markov models to generate new sentences
+from rhyme import rhyme_finder  # Use cmudict to find phonic rhymes
 import tqdm
 import os
 from nltk.tokenize import word_tokenize
@@ -10,34 +9,38 @@ from multiprocessing import Pool
 import string
 
 class Rapper:
-    """Ponicode Rapper generates lyrics by training on rap songs"""
+    """Ponicode Rapper generates lyrics by training on rap songs."""
 
     def __init__(self):
         self.data_dir = 'data'
         self.pools = 5
-        
+
     def remove_punctuation(self, word):
         return word.strip(string.punctuation)
-        
+
     def load_data(self, data_dir=None):
-        """
-            Input: String data dir
-            Output: String with all lyrics
+        """Produce lyrics from loaded data.
+        
+        Input: String data dir
+        Output: String with all lyrics
         """
         lyrics_data = ''
         if not data_dir:
             data_dir = self.data_dir
-        lyrics_files_paths = [f'{data_dir}/{file_path}' for file_path in os.listdir(data_dir) if '.txt' in file_path]
+        lyrics_files_paths = [f'{data_dir}/{file_path}'
+                              for file_path in os.listdir(data_dir)
+                              if '.txt' in file_path]
         for file_path in lyrics_files_paths:
             with open(file_path, 'r', encoding="utf8") as f:
                 lyrics_data += f.read() + '\n'
         self.tokenized_text = word_tokenize(lyrics_data)
         return lyrics_data
-        
+
     def build_space(self, lyrics_data, sentences_number=5000, state_size=2):
-        """ 
-            Input: String of lyrics with all data
-            Output: List of generated sentences of size sentences_number
+        """Produce sentences from loaded lyrics.
+        
+        Input: String of lyrics with all data
+        Output: List of generated sentences of size sentences_number
         """
         self.markov_model = markovify.NewlineText(lyrics_data, state_size)
         sentences = []
@@ -46,7 +49,7 @@ class Rapper:
             if line:
                 sentences.append(line)
         return sentences
-    
+
     def build_rhyme_dict(self, sentence):
         try:
             rhymes_dict = {}
@@ -54,26 +57,22 @@ class Rapper:
             last_word = self.remove_punctuation(last_word)
             rhymes_dict = {'line': sentence,
                             'last_word' : last_word,
-                            'rhymes': [last_word] + rhyme_finder(last_word, self.tokenized_text)
-                               }
+                            'rhymes': [last_word]
+                            + rhyme_finder(last_word,self.tokenized_text)
+                            }
             return rhymes_dict
-        except Exception as e:
+        except Exception:
             pass
             return None
-        
-    
+
     def build_rhyme_list(self, sentences, show_errors=False):
-        """
-            - Input: Generated sentences by markov model
-            - Output: List of dict with rhyme and lyrics data
-                    [{'line': 'All the pain I thought we could be',
-                     'last_word': 'be',
-                     'rhymes': ['be',
-                      'me',
-                      'somebody',
-                      'only',
-                    },
-                    ..]
+        """Produce rhymes from loaded sentences.
+        
+        Input: Generated sentences by Markov model
+        Output: List of dict with rhyme and lyrics data
+        [{'line': 'All the pain I thought we could be',
+          'last_word': 'be',
+          'rhymes': ['be','me', 'somebody', 'only', ...]}, ...]
         """
         rhymes_list = []
         poolmasters = Pool(self.pools)
@@ -83,40 +82,22 @@ class Rapper:
                 rhymes_list.append(result)
         poolmasters.terminate()
         return rhymes_list
-    
-    def build_equivalence_classes(self, rhymes_list):
-        """
-            - Input: List of dict with rhyme and lyrics data
-                    [{'line': 'All the pain I thought we could be',
-                     'last_word': 'be',
-                     'rhymes': ['be',
-                      'me',
-                      'somebody',
-                      'only',
-                    },
-                    ..]
-            - Output: List of dict of equivalent classes grouped by rhymes
-                    [
-                        {
-                            'be': [{'line': 'All the pain I thought we could be',
-                                   'last_word': 'be',
-                                   'rhymes': ['be',
-                                    'me',
-                                    'somebody',}
-                                    ....
-                                    ],
-                             'me': [{'line': 'Do you feel me?',
-                                      'last_word': 'me',
-                                      'rhymes': ['me',
-                                       'baby',
-                                       'somebody',
-                                       'only',
-                                       ...
-                                       ]
-                        },
-                        ...
 
-                    ..]
+    def build_equivalence_classes(self, rhymes_list):
+        """Produce rhyme equivalence classes from loaded rhymes and lyrics.
+        
+        Input: List of dict with rhyme and lyrics data
+        [{'line': 'All the pain I thought we could be',
+          'last_word': 'be',
+          'rhymes': ['be', 'me', 'somebody', 'only', ...]}, ...]
+        Output: List of dict of equivalent classes grouped by rhymes
+                    [{'be': [{'line': 'All the pain I thought we could be',
+                              'last_word': 'be',
+                              'rhymes': ['be', 'me', 'somebody', ...]}, ...],
+                      'me': [{'line': 'Do you feel me?',
+                              'last_word': 'me',
+                              'rhymes': ['me','baby','somebody','only',...]},
+                        ...]
         """
         equivalence_list = []
         k = 0
@@ -127,10 +108,10 @@ class Rapper:
             last_word_representor = representor['last_word']
             equivalence_list.append({last_word_representor: [representor]})
             rhymes_list = rhymes_list[1:]
-            # Loops over all sentences to group by rhymes and last word
+            # Loop over all sentences to group by rhymes and last word
             for i, rhyme_dict in enumerate(rhymes_list):
                 rhymes = rhyme_dict['rhymes']
-                if list(set(rhymes).intersection(representor['rhymes'])) != []:
+                if list(set(rhymes).intersection(representor['rhymes']))!=[]:
                     last_word = rhyme_dict['last_word']
                     # For never seen last word
                     if last_word not in equivalence_list[k].keys():
@@ -141,12 +122,14 @@ class Rapper:
                         rhymes_list.remove(rhyme_dict)
             k += 1
         return equivalence_list
-    
-    
-    def train(self, lyrics_data, sentences_number=5000, state_size=2, mod='artistic'):
-        """Trains model of equivalence classes from lyrics"""
+
+    def train(self, lyrics_data,
+              sentences_number=5000, state_size=2,
+              mod='artistic'):
+        """Train model of equivalence classes from lyrics"""
         print(f'Generating a space of {sentences_number} sentences')
-        sentences = self.build_space(lyrics_data, sentences_number, state_size)
+        sentences = self.build_space(lyrics_data,
+                                     sentences_number, state_size)
         self.free_style_sentences = sentences
         print(f'Grouping the sentences by rhymes')
         if mod == 'artistic':
@@ -155,30 +138,33 @@ class Rapper:
             equivalence_list = self.build_equivalence_classes(rhymes_list)
             print('Training Done')
             self.equivalence_list = equivalence_list
-    
-    
+
     def save_model(self, file_path):
         with open(file_path, 'wb') as file:
             pickle.dump(self, file, pickle.HIGHEST_PROTOCOL)
-            
+
     def load_model(self, file_path):
         with open(file_path, 'rb') as file:
             self.__dict__.update(pickle.load(file).__dict__)
-        
+
     def generate_random_pair(self, cluster):
         values_cluster = list(cluster.values())
         random.shuffle(values_cluster)
-        rhyme1_cluster, rhyme2_cluster = random.choice(values_cluster[0]), random.choice(values_cluster[1])
+        rhyme1_cluster = random.choice(values_cluster[0])
+        rhyme2_cluster = random.choice(values_cluster[1])
         return rhyme1_cluster['line'], rhyme2_cluster['line']
-    
+
     def generate_artistic_verses(self, N_VERSES=8):
         patterns = ['abab', 'aabb', 'abba']
         generated_lyrics = ''
-        equivalence_clusters = [cluster for cluster in self.equivalence_list if len(cluster.keys()) > 1]
+        equivalence_clusters = [cluster for cluster
+                                in self.equivalence_list
+                                if len(cluster.keys()) > 1]
         for _ in range(N_VERSES):
             pattern = random.choice(patterns)
             random.shuffle(equivalence_clusters)
-            cluster1, cluster2 = equivalence_clusters[0], equivalence_clusters[1]
+            cluster1 = equivalence_clusters[0]
+            cluster2 = equivalence_clusters[1]
             a1, a2 = self.generate_random_pair(cluster1)
             b1, b2 = self.generate_random_pair(cluster2)
             if pattern == 'abab':
@@ -191,12 +177,14 @@ class Rapper:
             generated_lyrics += '\n'
         generated_lyrics = self.replace_forbidden_words(generated_lyrics)
         return generated_lyrics
-    
+
     def generate_freestyle_verses(self, N_VERSES=8):
         verse_length = 4
-        index = random.choice(range(len(self.free_style_sentences)-N_VERSES*verse_length-1))
-        verses = self.free_style_sentences[index : index+N_VERSES*verse_length]
-        verses = [verses[i : i+verse_length] for i in range(0, len(verses), verse_length)]
+        index = random.choice(range(len(self.free_style_sentences)
+                                    -N_VERSES*verse_length-1))
+        verses = self.free_style_sentences[index:index+N_VERSES*verse_length]
+        verses = [verses[i:i+verse_length]
+                  for i in range(0, len(verses), verse_length)]
         generated_lyrics = ''
         for verse_set in verses:
             generated_lyrics += '\n'.join(verse_set)
@@ -204,10 +192,9 @@ class Rapper:
             generated_lyrics += '\n'
         generated_lyrics = self.replace_forbidden_words(generated_lyrics)
         return generated_lyrics
-    
+
     def replace_forbidden_words(self, generated_text):
         BAD_WORDS = [
-
             ['hitler','sniffler'],
             ['white', 'knight'],
             ['fuck', 'truck'],
@@ -222,7 +209,10 @@ class Rapper:
         for bad_word in BAD_WORDS:
             forbidden_word = bad_word[0]
             new_word = bad_word[1]
-            generated_text = generated_text.replace(forbidden_word.capitalize(), new_word)
-            generated_text = generated_text.replace(forbidden_word, new_word)
-            generated_text = generated_text.replace(forbidden_word.upper(), new_word)
+            generated_text=generated_text.replace(forbidden_word.capitalize(),
+                                                    new_word)
+            generated_text=generated_text.replace(forbidden_word,
+                                                    new_word)
+            generated_text=generated_text.replace(forbidden_word.upper(),
+                                                    new_word)
         return generated_text
